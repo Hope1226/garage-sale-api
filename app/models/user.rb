33d209b722b10dc65708a2d78ccf151d65babe07
Umidjon Ustabaev <umidjonustabaev@gmail.com>
2022-04-13 10:11:38 +0500

    Fix linters

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: self
-  
+
   validates :name, presence: { message: 'field can not be blank' }
 end
