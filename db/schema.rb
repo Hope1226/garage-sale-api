5f7d784569a228d7d346a213bdb72d450e00792d
Umidjon Ustabaev <umidjonustabaev@gmail.com>
2022-04-14 20:14:06 +0500

    Add new collumns to the product tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_14_143803) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_14_151110) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_14_143803) do
     t.bigint "store_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "stock", null: false
+    t.string "uom", null: false
     t.index ["store_id"], name: "index_products_on_store_id"
     t.index ["user_id"], name: "index_products_on_user_id"
   end
