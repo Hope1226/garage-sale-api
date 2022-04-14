a6384a24dfe81e0899866ca5fc2f3823dcc43542
Umidjon Ustabaev <umidjonustabaev@gmail.com>
2022-04-14 19:26:54 +0500

    Set up store controllers

---
+++ app/controllers/stores_controller.rb
@@ -0,0 +1,45 @@
+class StoresController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_store, only: %i[show update destroy]
+
+  # Get / Stores
+  def index
+    @stores = Store.all
+    render json: @stores
+  end
+
+  # Post / Stores
+  def create
+    @new_store = current_user.stores.create(store_params)
+
+    if @new_store.save
+      render json: @new_store, status: :created, location: @store
+    else
+      render json: @new_store.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH / PUT / stores / 1
+  def update
+    if @store.update(store_params)
+      render json: @store
+    else
+      render json: @store.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE / stores / 1
+  def destroy
+    @store.destroy
+  end
+
+  private
+
+  def set_store
+    @store = Store.find(params[:id])
+  end
+
+  def store_params
+    params.require(:store).permit(:name, :description)
+  end
+end
