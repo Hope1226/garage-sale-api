f3af8c42f415afa611d3edb67d50310f65475b22
Umidjon Ustabaev <umidjonustabaev@gmail.com>
2022-04-14 19:13:02 +0500

    Generate store model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
          :jwt_authenticatable, jwt_revocation_strategy: self
 
   validates :name, presence: { message: 'field can not be blank' }
+
+  has_many :stores
 end
