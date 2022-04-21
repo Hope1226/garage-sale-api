b2773de59cd512e28855fc5d64b5092bcc022b40
Umidjon Ustabaev <umidjonustabaev@gmail.com>
2022-04-21 19:54:08 +0500

    Add review association

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,4 +2,5 @@ class Customer < User
   has_many :orders, dependent: :destroy
   has_many :products, through: :orders
   has_many :ranks, dependent: :destroy
+  has_many :reviews, dependent: :destroy
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -11,4 +11,5 @@ class Product < ApplicationRecord
   has_many :orders, dependent: :destroy
   has_many :customers, through: :orders
   has_many :ranks, dependent: :destroy
+  has_many :reviews, dependent: :destroy
 end
