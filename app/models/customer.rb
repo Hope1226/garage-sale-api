f9e75521a77ae39b80f7ffabfeda41f6ac11b768
Umidjon Ustabaev <umidjonustabaev@gmail.com>
2022-04-21 22:50:24 +0500

    Set up respective associations

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,5 @@ class Customer < User
   has_many :products, through: :orders
   has_many :ranks, dependent: :destroy
   has_many :reviews, dependent: :destroy
+  has_many :conversations, dependent: :destroy
 end
