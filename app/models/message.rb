9700604a54935fb4bd66a6e4b81ac9ba0c91048b
Umidjon Ustabaev <umidjonustabaev@gmail.com>
2022-04-22 22:21:14 +0500

    Add validations to messages

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,6 @@
 class Message < ApplicationRecord
+  # Validations
+  validates :content, presence: { message: 'field can not be blank'}
   belongs_to :conversation
   belongs_to :seller
   belongs_to :customer
